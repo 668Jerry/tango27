b90760f0bb86bf30f0db3da4ce26fde5f7265e7f
jerry <668Jerry@gmail.com>
2014-06-27 14:10:35 +0800

    5.7finished debug

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -3,18 +3,19 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+# urlpatterns = patterns('',
+#     # Examples:
+#     # url(r'^$', 'tango_with_django_project.views.home', name='home'),
+#     # url(r'^blog/', include('blog.urls')),
+#
+#     url(r'^admin/', include(admin.site.urls)),
+# )
 
 from django.conf.urls import patterns, url
 from rango import views
-urlpatterns += patterns('',
+urlpatterns = patterns('',
     url(r'^$',views.index,name='index'),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 # At the top of your urls.py file, add the following line:
